17e6033faa473cbca7e6bdff9d11dcde902a5413
Khushi Bhardwaj <khushi.bhardwaj@gatech.edu>
2022-03-06 18:55:18 -0500

    Initial Commit

---
+++ PizzaAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaAnimation
+//
+//  Created by  Khushi Bhardwaj on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaAnimation/PizzaAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAnimationApp.swift
+//  PizzaAnimation
+//
+//  Created by  Khushi Bhardwaj on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
